af43bfe575cce4e8b3c5f445ec476d01e40977bf
febri <fitrafilthmongers@gmail.com>
2023-07-19 12:43:58 +0700

    tugas 14 js
    mengumpulkan tugas 14 js

---
+++ tugas14.js
@@ -0,0 +1,12 @@
+var nilai = 37/20;
+console.log(nilai)
+
+var round = Math.round(nilai);
+console.log("round", round)
+
+var ceil = Math.ceil(nilai);
+console.log("ceil", ceil)
+
+var floor = Math.floor(nilai);
+console.log("floor", floor)
+
